53584253f4a24304a3f96bb35b40fc9c548ec35e
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-02 14:25:27 +0900

    指定した月、その月の初日と最終日、またそれらの曜日を取得するメソッドを追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
+day_of_week = ['日 月 火 水 木 金 土']
+puts day_of_week
 
-DayOfWeek = ['日 月 火 水 木 金']
-
-puts DayOfWeek
+require 'date'
+date = Date.new(2025,5)
+first_day = Date.new(date.year, date.month, 1)
+last_day = Date.new(date.year, date.month, -1)
+puts first_day
+puts first_day.wday
+puts last_day
+puts last_day.wday
