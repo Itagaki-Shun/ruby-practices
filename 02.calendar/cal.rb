230281a59101dcb5353762cebe2112cec360d1cd
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-02 14:28:09 +0900

    繰り返し処理の追加（カレンダーの日付表示）

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/env ruby
 day_of_week = ['日 月 火 水 木 金 土']
 puts day_of_week
-
 require 'date'
 date = Date.new(2025,5)
 first_day = Date.new(date.year, date.month, 1)
 last_day = Date.new(date.year, date.month, -1)
-puts first_day
-puts first_day.wday
-puts last_day
-puts last_day.wday
+
+(1..last_day.day).each do |day|
+    print day
+end
