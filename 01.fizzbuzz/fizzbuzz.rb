2e17ece1ec28b6ae3aaf69ad5676d43e850aed31
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-02 10:18:24 +0900

    条件分岐とそれぞれの処理を追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,4 +1,13 @@
 #!/usr/bin/env ruby
 for i in (1..20)
-    puts i
+    case
+    when i % 3 === 0 && i % 5 === 0
+        puts "FizzBuzz"
+    when i % 3 === 0
+        puts "Fizz"
+    when i % 5 === 0
+        puts "Buzz"
+    else
+        puts i
+    end
 end
