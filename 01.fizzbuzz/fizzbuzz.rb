00be81017be1400654afd2e475621cc19a166c9f
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-08 08:56:31 +0900

    コードのインデント修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 (1..20).each do |i|
-if i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz"
+    if i % 3 == 0 && i % 5 == 0
+        puts "FizzBuzz"
     elsif i % 3 == 0
         puts "Fizz"
     elsif i % 5 == 0
