35eb304583a1abc22cf84ef81b6a6098ff921090
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-07 09:47:11 +0900

    条件分岐処理を修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
 (1..20).each do |i|
-    case
-    when i % 3 === 0 && i % 5 === 0
+    if i % 3 === 0 && i % 5 === 0
         puts "FizzBuzz"
-    when i % 3 === 0
+    elsif i % 3 === 0
         puts "Fizz"
-    when i % 5 === 0
+    elsif i % 5 === 0
         puts "Buzz"
     else
         puts i
