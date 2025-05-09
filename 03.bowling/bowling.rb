b0fc730e04cde4f1d4ef86e3fac0167a4b0215ec
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-09 16:07:22 +0900

    スコア計算プログラム作成

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -13,4 +13,28 @@ scores.each_slice(2) do |i|
     frames.push(i)
 end
 
+point = 0
+
+frames.each_with_index do |frame,index|
+    if index < 9
+        if frame[0] == 10 #ストライク
+            point += 10 + frames[index + 1][0] + frames[index + 1][1]
+        elsif frame.sum == 10
+            point += 10 + frames[index + 1][0]
+        else
+            point += frame.sum
+        end
+    end
+    if index == 9
+        if frame[0] == 10 #ストライク
+            point += 10 + frames[index + 1][0] + frames[index + 1][1]
+        elsif frame.sum == 10
+            point += 10 + frames[index + 1][0]
+        else
+            point += frame.sum
+        end
+    end
+end
+puts point
+
 
