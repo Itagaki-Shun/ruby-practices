bf39e6e504f81edbb94846ec4e30b3098adfb0df
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-13 11:19:21 +0900

    変数名を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,20 +9,20 @@ opt.on('-y [VAL]',Integer) {|v| option[:y] = v }
 opt.on('-m [VAL]',Integer) {|v| option[:m] = v }
 opt.parse!(ARGV)
 
-argument_month = nil
-argument_year = nil
+month = nil
+year = nil
 # -mと-yに値があるかの確認、なければ今月を代入
 if option[:y]
-    argument_year = option[:y]
+    year = option[:y]
 else
-    argument_year = Date.today.year
+    year = Date.today.year
 end
 if option[:m]
-    argument_month = option[:m]
+    month = option[:m]
 else
-    argument_month = Date.today.month
+    month = Date.today.month
 end
-date = Date.new(argument_year,argument_month)
+date = Date.new(year,month)
 
 first_day = Date.new(date.year, date.month, 1)
 last_day = Date.new(date.year, date.month, -1)
@@ -33,7 +33,6 @@ puts day_of_week
 
 # 初日の位置を決める処理（日曜日であればスペースは空けない）
 print (first_day.wday == 0 ? '' : '  ' + '   ' * (first_day.wday - 1))
-
 (first_day.day..last_day.day).each do |day|
     current_date = Date.new(date.year,date.month,day)
     if current_date.wday > 0
