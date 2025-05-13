83ffa7209b208930a52a4ca733d7a8940697241b
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-13 10:47:22 +0900

    次のフレームもストライクかどうか判定する処理を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,33 +1,37 @@
 #!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
 scores = []
 frames = []
 ARGV.each do |arg|
-    scores = arg.split(',')
+  scores = arg.split(',')
 end
 
 scores = scores.flat_map do |x|
-        x == 'X' ? [10,0] : [x.to_i]
+  x == 'X' ? [10, 0] : [x.to_i]
 end
 scores.each_slice(2) do |score|
-    frames.push(score)
+  frames.push(score)
 end
 
 point = 0
-frames.each_with_index do |frame,index|
-    if index < 9 # 9フレームまでの計算
-        if frame[0] == 10 #ストライク
-            if frames[index + 1].include?(10) #次のフレームもストライクか判定
-                point += 10 + frames[index + 1][0] + frames[index + 2][0]
-            else
-                point += 10 + frames[index + 1][0] + frames[index + 1][1]
-            end
-        elsif frame.sum == 10 # スペア
-            point += 10 + frames[index + 1][0]
-        else
-            point += frame.sum
-        end
-    else #最終フレームの計算
-        point += frame.sum
+frames.each_with_index do |frame, index|
+  if index < 9 # 9フレームまでの計算
+    if frame[0] == 10 # ストライク
+      is_next_strike = frames[index + 1].include?(10) # 次のフレームもストライクか判定
+      point += if is_next_strike
+                 10 + frames[index + 1][0] + frames[index + 2][0]
+               else
+                 point += 10 + frames[index + 1][0] + frames[index + 1][1]
+               end
+    elsif frame.sum == 10 # スペア
+      point += 10 + frames[index + 1][0]
+    else
+      point += frame.sum
     end
+  else # 最終フレームの計算
+    point += frame.sum
+  end
 end
 puts point
