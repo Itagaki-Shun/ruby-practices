21a425b8883e6109d6b8006fb39f83bc10bbe058
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-13 09:42:36 +0900

    各フレームごとの得点に分ける処理の修正、スコア計算の処理を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,39 +5,29 @@ ARGV.each do |arg|
     scores = arg.split(',')
 end
 
-scores = scores.flat_map.with_index do |x,i|
-    if i < 16
-        x == 'X' ? [10,0] : [x.to_i] if i < 16
-    # 10フレーム目にストライクやスペアがあったときは10のみ入れる
-    else
-        x == 'X' ? [10] :[x.to_i]
-    end
+scores = scores.flat_map do |x|
+        x == 'X' ? [10,0] : [x.to_i]
 end
-
 scores.each_slice(2) do |score|
     frames.push(score)
 end
 
 point = 0
-
 frames.each_with_index do |frame,index|
-    if index < 9
+    if index < 9 # 9フレームまでの計算
         if frame[0] == 10 #ストライク
-            if frames[index + 1].include?(10)
-                # binding.irb
+            if frames[index + 1].include?(10) #次のフレームもストライクか判定
                 point += 10 + frames[index + 1][0] + frames[index + 2][0]
             else
                 point += 10 + frames[index + 1][0] + frames[index + 1][1]
             end
-        elsif frame.sum == 10
+        elsif frame.sum == 10 # スペア
             point += 10 + frames[index + 1][0]
         else
             point += frame.sum
         end
-    else
+    else #最終フレームの計算
         point += frame.sum
     end
 end
 puts point
-
-
