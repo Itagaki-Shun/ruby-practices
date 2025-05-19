61af7439062695a29bb08692c5ea9573e72f0a36
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-19 09:45:02 +0900

    スコア計算の処理を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -12,22 +12,21 @@ end
 
 frames = scores.each_slice(2).to_a
 
-point = 0
-frames.each_with_index do |frame, index|
+point = frames.each_with_index.sum do |frame, index|
   if index < 9 # 9フレームまでの計算
     if frame[0] == 10 # ストライク
-      point = if frames[index + 1].include?(10) # 次のフレームもストライクか判定
-                point + 10 + frames[index + 1][0] + frames[index + 2][0]
-              else
-                point + 10 + frames[index + 1][0] + frames[index + 1][1]
-              end
+      if frames[index + 1][0] == 10 # 次のフレームもストライクか判定
+        10 + frames[index + 1][0] + frames[index + 2][0]
+      else
+        10 + frames[index + 1][0] + frames[index + 1][1]
+      end
     elsif frame.sum == 10 # スペア
-      point += 10 + frames[index + 1][0]
+      10 + frames[index + 1][0]
     else
-      point += frame.sum
+      frame.sum
     end
   else # 最終フレームの計算
-    point += frame.sum
+    frame.sum
   end
 end
 puts point
