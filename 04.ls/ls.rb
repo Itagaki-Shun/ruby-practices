7801ac98dbdc5bdeef64cac34b0246203d15709c
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-20 13:28:21 +0900

    三列の出力ができる処理を追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,4 +6,16 @@ file = Dir.entries(path)
 file.delete_if { |str| str.start_with?('.') }
 
 file = file.sort_by(&:downcase)
-p file
+
+columns = 3
+rows = (file.size.to_f / columns).ceil
+transformation_file = Array.new(rows) { Array.new(columns) }
+
+file.each_with_index do |name, index|
+  row = index % rows
+  col = index / rows
+  transformation_file[row][col] = name
+end
+transformation_file.each_with_index do |row|
+  puts row.compact.join('    ')
+end
