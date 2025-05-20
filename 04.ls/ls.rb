79daadab19ac323f054930f8f3feaacf7fff5d56
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-20 15:34:10 +0900

    不要な変数の削除、変数名の変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/env ruby
 
-path = "/home/itagaki_syun/ruby-practices/04.ls"
-file = Dir.entries(path)
+PATH = '/home/itagaki_syun/ruby-practices/04.ls'.freeze
+file = Dir.entries(PATH)
 
 file = file.reject { |str| str.start_with?('.') }.sort_by(&:downcase)
 
 columns = 3
 rows = (file.size.to_f / columns).ceil
 transformation_file = Array.new(rows) { Array.new(columns) }
-file_name_length = file.max_by{ |str| str.to_s.length}
-space =  file_name_length.length
+file_name_length = file.max_by { |name| name.to_s.length }
 
 file.each_with_index do |name, index|
   row = index % rows
   col = index / rows
-  transformation_file[row][col] = name.ljust(space + 1)
+  transformation_file[row][col] = name.ljust(file_name_length.length + 1)
 end
 
 transformation_file.each do |row|
