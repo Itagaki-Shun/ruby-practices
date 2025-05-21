a32e4045b259e245ce4a2608072091efe6c6b1db
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-21 11:20:51 +0900

    ファイルやディレクトリを指定した形で出力できるように変換し、出力する処理を行うメソッドを追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,19 +9,23 @@ def file_sort(file)
   file.reject { |str| str.start_with?('.') }.sort_by(&:downcase)
 end
 
+# ファイルやディレクトリを指定した形で出力できるように変換し、出力する処理
+def transformation_output_file(file)
+  columns = 3
+  rows = (file.size.to_f / columns).ceil
+  transformation_file = Array.new(rows) { Array.new(columns) }
+  file_name_length = file.max_by { |name| name.to_s.length }
+
+  file.each_with_index do |name, index|
+    row = index % rows
+    col = index / rows
+    transformation_file[row][col] = name.ljust(file_name_length.length + 1) unless col == columns
+  end
+
+  transformation_file.each do |row|
+    puts row.compact.join
+  end
+end
+
 file = file_sort(file)
-
-columns = 3
-rows = (file.size.to_f / columns).ceil
-transformation_file = Array.new(rows) { Array.new(columns) }
-file_name_length = file.max_by { |name| name.to_s.length }
-
-file.each_with_index do |name, index|
-  row = index % rows
-  col = index / rows
-  transformation_file[row][col] = name.ljust(file_name_length.length + 1) unless col == columns
-end
-
-transformation_file.each do |row|
-  puts row.compact.join
-end
+transformation_output_file(file)
