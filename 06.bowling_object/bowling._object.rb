ec63958d8921167000496d49b58a6279419f8f47
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-22 14:41:11 +0900

    ボウリングのスコア計算プログラムを実装

--- 06.bowling_object/bowling._object.rb
+++ 06.bowling_object/bowling._object.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
 
+require_relative 'shot'
+require_relative 'frame'
+require_relative 'game'
+
+if ARGV.length.positive?
+  shot = Shot.new(ARGV[0])
+  frame = Frame.new(shot.score_per_throw)
+  game = Game.new(frame.frame_by_frame_score)
+  p game.scoring
+else
+  puts 'スコアを入力してください'
+end
