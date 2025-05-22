6e024b1516a8187b8d2aa111ae203500c10934bb
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-22 11:54:55 +0900

    引数を一投毎のスコアに分けるメソッド（score_per_throw）の追加、initializeメソッドに追記、テストケースの追加

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/env ruby
 
 class Shot
-  attr_reader :scores
+  attr_reader :scores, :score_per_throw
 
   def initialize(pin = ARGV[0])
     @scores = pin.split(',')
+    @score_per_throw = score_per_throw
+  end
+
+  def score_per_throw
+    @scores.flat_map do |x|
+      x == 'X' ? [10,0] : [x.to_i]
+    end
   end
 end
--- 06.bowling_object/test_bowling.rb
+++ 06.bowling_object/test_bowling.rb
@@ -7,11 +7,14 @@ class BowlingTest < Minitest::Test
   def test_shot
     shot_1 = Shot.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
     assert_equal ["6", "3", "9", "0", "0", "3", "8", "2", "7", "3", "X", "9", "1", "8", "0", "X", "6", "4", "5"], shot_1.scores
+    assert_equal [6, 3, 9, 0, 0, 3, 8, 2, 7, 3, 10, 0, 9, 1, 8, 0, 10, 0, 6, 4, 5], shot_1.score_per_throw
 
     shot_2 = Shot.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
     assert_equal ["6", "3", "9", "0", "0", "3", "8", "2", "7", "3", "X", "9", "1", "8", "0", "X", "X", "X", "X"], shot_2.scores
+    assert_equal [6, 3, 9, 0, 0, 3, 8, 2, 7, 3, 10, 0, 9, 1, 8, 0, 10, 0, 10, 0, 10, 0, 10, 0], shot_2.score_per_throw
 
     shot_3 = Shot.new('X,X,X,X,X,X,X,X,X,X,X,X')
     assert_equal ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"], shot_3.scores
+    assert_equal [10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0], shot_3.score_per_throw
   end
 end
