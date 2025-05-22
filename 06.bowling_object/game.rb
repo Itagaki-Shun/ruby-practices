edaf89c514c79458bec61464ab90472e7c99b80d
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-22 14:11:20 +0900

    スコアを計算するメソッドscoringを追加、initializeメソッドを定義

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby
 
 class Game
+  def initialize(frames)
+    @frames = frames
+  end
+
+  def scoring
+    point = @frames.each_with_index.sum do |frame, index|
+      point = frame.sum
+
+      if index < 9 # 9フレームまでの計算
+        if frame[0] == 10 # ストライク
+          point += if @frames[index + 1][0] == 10 # 次のフレームもストライクか判定
+                    @frames[index + 1][0] + @frames[index + 2][0]
+                  else
+                    @frames[index + 1].sum
+                  end
+        elsif frame.sum == 10 # スペア
+          point += @frames[index + 1][0]
+        end
+      end
+
+      point
+    end
+  end
 end
