58d8f58fe29f12107c305cc15dc8d437514fed8c
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-23 10:24:15 +0900

    不要な変数を削除、不要なattr_readerメソッドを削除、コメントの追加、score_per_throwメソッドを修正

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 class Shot
-  attr_reader :scores, :score_per_throw
+  attr_reader :scores # テスト用のattr_reader
 
   def initialize(pin = ARGV[0])
     @scores = pin.split(',')
-    @score_per_throw = score_per_throw
   end
 
   def score_per_throw
     @scores.flat_map do |x|
-      x == 'X' ? [10,0] : [x.to_i]
+      x == 'X' ? [10, 0] : [x.to_i]
     end
   end
 end
