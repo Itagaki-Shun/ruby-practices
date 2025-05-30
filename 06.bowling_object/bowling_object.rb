9371c50b6da17c1bc98e404ab24847fb25a7a593
itagaki <itagaki_shun@vitallead.co.jp>
2025-05-30 09:30:30 +0900

    ファイル名を修正、出力処理を修正

--- 06.bowling_object/bowling_object.rb
+++ 06.bowling_object/bowling_object.rb
@@ -10,7 +10,7 @@ if ARGV.length.positive?
   shot = Shot.new(ARGV[0])
   frame = Frame.new(shot.score_per_throw)
   game = Game.new(frame.frame_by_frame_score)
-  p game.scoring
+  puts game.scoring
 else
   puts 'スコアを入力してください'
 end
