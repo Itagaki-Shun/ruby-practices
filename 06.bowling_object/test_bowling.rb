e35a99c2f398c3f664e4755f836d41e36c08853e
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-04 10:17:21 +0900

    Frameクラスのテストを変更

--- 06.bowling_object/test_bowling.rb
+++ 06.bowling_object/test_bowling.rb
@@ -23,17 +23,22 @@ class BowlingTest < Minitest::Test
   end
 
   def test_frame
-    first_shot = Shot.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
-    first_frame = Frame.new(first_shot.score_per_throw)
-    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10, 0], [9, 1], [8, 0], [10, 0], [6, 4], [5]], first_frame.frame_by_frame_score
+    frame = Frame.new([6, 3])
+    assert_equal 9, frame.score
+    assert_equal 6, frame.first_roll
+    assert_equal 3, frame.second_roll
 
-    second_shot = Shot.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
-    second_frame = Frame.new(second_shot.score_per_throw)
-    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10, 0], [9, 1], [8, 0], [10, 0], [10, 0], [10, 0], [10, 0]], second_frame.frame_by_frame_score
+    strike_frame = Frame.new([10, 0])
+    assert strike_frame.strike?
+    refute strike_frame.spare?
 
-    third_shot = Shot.new('X,X,X,X,X,X,X,X,X,X,X,X')
-    third_frame = Frame.new(third_shot.score_per_throw)
-    assert_equal [[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0]], third_frame.frame_by_frame_score
+    spare_frame = Frame.new([6, 4])
+    assert spare_frame.spare?
+    refute spare_frame.strike?
+
+    spare_or_strike_frame = Frame.new([0, 10])
+    refute spare_or_strike_frame.strike?
+    assert spare_or_strike_frame.spare?
   end
 
   def test_game
