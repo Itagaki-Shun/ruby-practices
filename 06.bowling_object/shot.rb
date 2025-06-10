91e5fc6d48b52fc12300e8f9cd16fcbc60e93c25
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-10 10:00:24 +0900

    Shotクラスを修正

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 class Shot
-  attr_reader :scores # テスト用のattr_reader
+  attr_reader :pins # テスト用のattr_reader
 
-  def initialize(pin = ARGV[0])
-    @scores = pin.split(',')
-  end
-
-  def score_per_throw
-    @scores = @scores.flat_map do |x|
-      x == 'X' ? [10, 0] : [x.to_i]
-    end
-    @scores.each_slice(2).to_a
+  def initialize(pin_count)
+    @pins = pin_count == 'X' ? 10 : pin_count.to_i
   end
 end
