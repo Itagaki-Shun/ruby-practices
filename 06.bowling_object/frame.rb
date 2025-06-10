5dab285697372dd35bf83d02115546778e184297
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-10 13:22:06 +0900

    Frameクラスに1~10フレームを生成するbuild_framesメソッドと、スコア計算を行うtotal_scoreメソッドを追加

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -7,6 +7,25 @@ class Frame
     @shots = shots
   end
 
+  def self.build_frames(rolls)
+    frames = []
+    i = 0
+
+    9.times do
+      if rolls[i].pins == 10
+        frames << new([rolls[i]])
+        i += 1
+      else
+        frames << new(rolls[i], rolls[i + 1])
+        i += 2
+      end
+    end
+
+    last_frame = rolls[i..] || []
+    frames << new(*last_frame)
+    frames
+  end
+
   def score
     shots.sum(&:pins)
   end
@@ -17,5 +36,21 @@ class Frame
 
   def spare?
     shots.size >= 2 && shots[0].pins + shots[1].pins == 10
+  end
+
+  def total_score(next_frames)
+    return score if next_frames.empty?
+
+    point = score
+
+    if strike?
+      bonus = next_frames.flat_map(&:shots)
+      point + bonus[0].pins + bonus[1].pins
+    elsif spare?
+      bonus = next_frames.flat_map(&:shots)
+      point + bonus[0].pins
+    else
+      point
+    end
   end
 end
