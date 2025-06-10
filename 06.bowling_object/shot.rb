2157c535caa289a55a9793c455b8735f13e77c8f
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-10 13:40:52 +0900

    不要なコメントを削除

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Shot
-  attr_reader :pins # テスト用のattr_reader
+  attr_reader :pins
 
   def initialize(pin_count)
     @pins = pin_count == 'X' ? 10 : pin_count.to_i
