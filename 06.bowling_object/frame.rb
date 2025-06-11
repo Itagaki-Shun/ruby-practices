7e58b319820c44b5f284f0d55f0b208acf59b8df
itagaki <itagaki_shun@vitallead.co.jp>
2025-06-11 13:37:20 +0900

    Frameクラスのbuild_framesメソッドを修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -8,22 +8,23 @@ class Frame
   end
 
   def self.build_frames(rolls)
-    frames = []
     i = 0
 
-    9.times do
-      if rolls[i].pins == 10
-        frames << new([rolls[i]])
-        i += 1
-      else
-        frames << new(rolls[i], rolls[i + 1])
-        i += 2
+    frames = Array.new(10) do |frame_idx|
+      if frame_idx < 9 # 9フレームまで
+        if rolls[i].pins == 10
+          frames = new([rolls[i]])
+          i += 1
+        else
+          frames = new(rolls[i], rolls[i + 1])
+          i += 2
+        end
+        frames
+      else # 最終フレーム
+        last_frame = rolls[i..] || []
+        new(*last_frame)
       end
     end
-
-    last_frame = rolls[i..] || []
-    frames << new(*last_frame)
-    frames
   end
 
   def score
