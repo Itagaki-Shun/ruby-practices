b70ee65098bac2f0a4e0b0271c2239fef44d78a2
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-04 16:27:55 +0900

    コマンドのオプション機能を追加 機能追加
    [目的]
    - OS標準のwcコマンド同様にオプションを付けることで必要な情報を出力させる
    
    [変更内容]
    - -l（行数）、-w（単語数）、-c（文字数）オプションを追加
    - 複数オプションの組み合わせに対応
    - 標準入力とファイル入力で出力フォーマットを調整

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -11,20 +11,29 @@ OptionParser.new do |opts|
   opts.on('-c') { options[:characters] = true }
 end.parse!
 
-def file_statistics(read_file)
+def file_statistics(read_file, options)
   lines = read_file.lines.count
   words = read_file.split
   words_count = words.length
   characters = read_file.bytesize
-
-  result = ["  #{lines}  ", words_count, characters].join(' ')
+  result = []
+  if ARGV.empty?
+    result << lines.to_s.rjust(7) if options[:lines] || options.empty?
+    result << words_count.to_s.rjust(8) if options[:words] || options.empty?
+    result << characters.to_s.rjust(8) if options[:characters] || options.empty?
+  else
+    result << lines.to_s.rjust(4) if options[:lines] || options.empty?
+    result << words_count.to_s.rjust(5) if options[:words] || options.empty?
+    result << characters.to_s.rjust(5) if options[:characters] || options.empty?
+  end
+  result.join
 end
 
 if ARGV.empty?
   read_file = $stdin.read
-  puts file_statistics(read_file)
+  puts file_statistics(read_file, options)
 else
   file = File.open(ARGV[0])
   read_file = File.read(file)
-  puts "#{file_statistics(read_file)} #{file.path}"
+  puts "#{file_statistics(read_file, options)} #{file.path}"
 end
