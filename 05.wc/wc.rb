430cc2317bd2258ba49d7dabf606f2b534dcc1a1
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-04 10:55:40 +0900

    file_statistics関数を実装 機能追加
    [目的]
    - ファイル統計計算を行う
    
    [変更内容]
    - 行数、単語数、バイト数を計算
    - フォーマットした結果を返す
    - メイン処理で関数を呼び出し

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,4 +4,12 @@ file = File.open(ARGV[0])
 read_file = File.read(file)
 
 def file_statistics(read_file)
+  lines = read_file.lines.count
+  words = read_file.split
+  words_count = words.length
+  characters = read_file.bytesize
+
+  result = ["  #{lines}  ", words_count, characters].join(' ')
 end
+
+puts "#{file_statistics(read_file)} #{file.path}"
