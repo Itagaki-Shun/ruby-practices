dfe2fe67c94a4662baab119e88d0d7d45d34eaf0
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-10 16:31:33 +0900

    LsOptionクラスにコマンドラインオプションを解析する機能を追加 機能追加
    [目的]
    - コマンドラインオプションの解析ロジックをカプセル化
    
    [変更内容]
    - optparseを使用して、`-a`、`-r`、`-l`オプションを解析する機能を実装
    - 解析結果を問い合わせるためのメソッド(`all?`、`reverse?`、`long_format?`)を追加

--- 07.ls_object/ls_option.rb
+++ 07.ls_object/ls_option.rb
@@ -2,5 +2,33 @@
 
 # frozen_string_literal: true
 
+require 'optparse'
+
 class LsOption
+  attr_reader :options
+
+  def initialize(args = ARGV)
+    @options = {}
+    parse(args)
+  end
+
+  def parse(args)
+    OptionParser.new do |opts|
+      opts.on('-a') { @options[:all] = true }
+      opts.on('-r') { @options[:reverse] = true }
+      opts.on('-l') { @options[:long_format] = true }
+    end.parse!(args)
+  end
+
+  def all?
+    @options[:all]
+  end
+
+  def reverse?
+    @options[:reverse]
+  end
+
+  def long_format?
+    @options[:long_format]
+  end
 end
