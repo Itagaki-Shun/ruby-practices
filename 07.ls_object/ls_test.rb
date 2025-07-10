f122be29ca082dc66b3dd7ae935cdae854bf1ab7
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-10 16:25:10 +0900

    LsOptionクラスのテストを追加 テスト追加
    [目的]
    - LsOptionクラスの動作確認
    
    [変更内容]
    - LsTestクラスにtest_ls_option_singularメソッドとtest_ls_option_multipleメソッドを追加
    - 単数オプション(-a, -r, -l)が渡された時のテストケースを追加
    - 複数オプション(-ar, -al, -arl)が渡された時のテストケースを追加

--- 07.ls_object/ls_test.rb
+++ 07.ls_object/ls_test.rb
@@ -3,6 +3,48 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require_relative 'ls_option'
 
 class LsTest < Minitest::Test
+  # 単数の場合
+  def test_ls_option_singular
+    # allオプションの場合
+    option_a = LsOption.new(['-a'])
+    assert option_a.all?
+    refute option_a.reverse?
+    refute option_a.long_format?
+
+    # reverseオプションの場合
+    option_r = LsOption.new(['-r'])
+    refute option_r.all?
+    assert option_r.reverse?
+    refute option_r.long_format?
+
+    # long_formatオプションの場合
+    option_l = LsOption.new(['-l'])
+    refute option_l.all?
+    refute option_l.reverse?
+    assert option_l.long_format?
+  end
+
+  # 複数オプションの場合
+  def test_ls_option_multiple
+    # allとreverse
+    option_ar = LsOption.new(['-ar'])
+    assert option_ar.all?
+    assert option_ar.reverse?
+    refute option_ar.long_format?
+
+    # allとlong_format
+    option_al = LsOption.new(['-al'])
+    assert option_al.all?
+    refute option_al.reverse?
+    assert option_al.long_format?
+
+    # 全部
+    option_arl = LsOption.new(['-arl'])
+    assert option_arl.all?
+    assert option_arl.reverse?
+    assert option_arl.long_format?
+  end
 end
