a99091e9963a2dc5c74ddbd1543b4f8818e1836c
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-14 16:21:58 +0900

    出力フォーマットの桁揃えを行う処理を修正 リファクタリング
    [目的]
    - 出力フォーマットの桁揃えを、ファイルごとや合計値の最大桁数に基づいて動的に計算するように改善する
    
    [変更内容]
    - `column_widths`メソッドを`select_widths`に改名し、オプションに応じた最終的な幅を決定する処理を行う
    - `calc_max_widths`メソッドを追加し、全ファイルの統計情報を事前に計算して最大桁数を算出する処理を行う
    - `file_statistics`と`format_statistics`メソッドが、計算された桁幅を受け取るように変更
    - メインロジックで`calc_max_widths`を先に呼び出し、その結果を各メソッドに渡すように修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -11,22 +11,20 @@ OptionParser.new do |opts|
   opts.on('-c') { options[:characters] = true }
 end.parse!
 
-def file_statistics(read_file, options, total_statistics = nil, is_total: false)
+def file_statistics(read_file, options, widths, total_statistics = nil, is_total: false)
   if is_total
     sum_totals = total_statistics.transpose.map(&:sum)
-    format_statistics(*sum_totals, options).join
+    format_statistics(*sum_totals, widths, options).join(' ')
   else
     lines = read_file.lines.count
     words = read_file.split.count
     characters = read_file.bytesize
     total_statistics << [lines, words, characters]
-    format_statistics(lines, words, characters, options).join
+    format_statistics(lines, words, characters, widths, options).join(' ')
   end
 end
 
-def format_statistics(lines, words, characters, options)
-  widths = column_widths(options)
-
+def format_statistics(lines, words, characters, widths, options)
   values = [lines, words, characters]
   keys = %i[lines words characters]
 
@@ -35,37 +33,52 @@ def format_statistics(lines, words, characters, options)
             else
               values.select.with_index { |_, i| options[keys[i]] }
             end
-  results.map.with_index { |value, index| value.to_s.rjust(widths[index]) }
+  results.map { |value| value.to_s.rjust(widths) }
 end
 
-def column_widths(options)
-  if ARGV.empty?
-    if options.values.count(true) == 1
-      [0, 0, 0]
-    else
-      [7, 8, 8]
-    end
-  elsif options.values.count(true) == 1
-    [0, 0, 0]
+def select_widths(max_width, options)
+  if options.values.count(true) == 1
+    keys = %i[lines words characters]
+    active_key = keys.find { |key| options[key] }
+    max_width[active_key]
   else
-    [4, 5, 5]
+    ARGV.empty? ? 7 : 4
   end
 end
+
+def calc_max_widths(lists, options)
+  all_statistics = []
+  keys = %i[lines words characters]
+  lists.each do |filename|
+    file = File.open(filename)
+    read_file = File.read(file)
+    all_statistics << [read_file.lines.count, read_file.split.count, read_file.bytesize]
+  end
+  totals = all_statistics.transpose.map(&:sum)
+  all_with_totals = all_statistics + [totals]
+  max_width = keys.zip(all_with_totals.transpose.map { |column| column.max.to_s.length }).to_h
+
+  return options.values.count(true) == 1 ? 0 : 7 if all_statistics.empty?
+
+  select_widths(max_width, options)
+end
+
+max_widths = calc_max_widths(ARGV, options)
 
 case ARGV.length
 when 0
   read_file = $stdin.read
-  puts file_statistics(read_file, options, [])
+  puts file_statistics(read_file, options, max_widths, [])
 when 1
   file = File.open(ARGV[0])
   read_file = File.read(file)
-  puts "#{file_statistics(read_file, options, [])} #{file.path}"
+  puts "#{file_statistics(read_file, options, max_widths, [])} #{file.path}"
 else
   statistics = []
   ARGV.each_with_index do |filename, index|
     file = File.open(filename)
     read_file = File.read(file)
-    puts "#{file_statistics(read_file, options, statistics)} #{file.path}"
-    puts "#{file_statistics(nil, options, statistics, is_total: true)} 合計" if index == ARGV.length - 1
+    puts "#{file_statistics(read_file, options, max_widths, statistics)} #{file.path}"
+    puts "#{file_statistics(nil, options, max_widths, statistics, is_total: true)} 合計" if index == ARGV.length - 1
   end
 end
