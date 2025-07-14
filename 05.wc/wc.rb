d3940f5988cc4891f79ef4935378ea0f396e45b2
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-14 11:20:51 +0900

    合計値計算の処理を修正 リファクタリング
    [目的]
    - 複数ファイル指定時の合計値計算処理を効率化し、コードの可読性を向上させる。
    
    [変更内容]
    - `calc_file_statistics_totals`メソッドを削除し、そのロジックを`file_statistics`メソッドに統合
    - `file_statistics`メソッドにキーワード引数`is_total`を追加、合計値計算の分岐を明確化
    - 単語数(`words`)の計算を統一

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -11,28 +11,24 @@ OptionParser.new do |opts|
   opts.on('-c') { options[:characters] = true }
 end.parse!
 
-def file_statistics(read_file, options)
-  lines = read_file.lines.count
-  words = read_file.split
-  words_count = words.length
-  characters = read_file.bytesize
-
-  format_statistics(lines, words_count, characters, options).join
-end
-
-def calc_file_statistics_totals(statistics, options)
-  total_statistics = statistics.map do |str|
-    str.strip.split.map(&:to_i)
+def file_statistics(read_file, options, total_statistics = nil, is_total: false)
+  if is_total
+    sum_totals = total_statistics.transpose.map(&:sum)
+    format_statistics(*sum_totals, options).join
+  else
+    lines = read_file.lines.count
+    words = read_file.split.count
+    characters = read_file.bytesize
+    total_statistics << [lines, words, characters]
+    format_statistics(lines, words, characters, options).join
   end
-  sum_totals = total_statistics.transpose.map(&:sum)
-  format_statistics(sum_totals[0], sum_totals[1], sum_totals[2], options).join
 end
 
-def format_statistics(lines, words_count, characters, options)
+def format_statistics(lines, words, characters, options)
   results = []
   widths = column_widths(options)
   results << lines if options[:lines] || options.empty?
-  results << words_count if options[:words] || options.empty?
+  results << words if options[:words] || options.empty?
   results << characters if options[:characters] || options.empty?
   results.map.with_index { |value, index| value.to_s.rjust(widths[index]) }
 end
@@ -54,18 +50,17 @@ end
 case ARGV.length
 when 0
   read_file = $stdin.read
-  puts file_statistics(read_file, options)
+  puts file_statistics(read_file, options, [])
 when 1
   file = File.open(ARGV[0])
   read_file = File.read(file)
-  puts "#{file_statistics(read_file, options)} #{file.path}"
+  puts "#{file_statistics(read_file, options, [])} #{file.path}"
 else
   statistics = []
   ARGV.each_with_index do |filename, index|
     file = File.open(filename)
     read_file = File.read(file)
-    puts "#{file_statistics(read_file, options)} #{file.path}"
-    statistics << file_statistics(read_file, options)
-    puts "#{calc_file_statistics_totals(statistics, options)} 合計" if index == ARGV.length - 1
+    puts "#{file_statistics(read_file, options, statistics)} #{file.path}"
+    puts "#{file_statistics(nil, options, statistics, is_total: true)} 合計" if index == ARGV.length - 1
   end
 end
