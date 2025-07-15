72497a975d7756a2492d6dd90800dc1d2d0e4d13
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-15 13:29:13 +0900

    Permクラスに定数を追加 機能追加
    [目的]
    - 対応したファイルタイプとパーミッションの文字列に変換を行う
    
    [変更内容]
    - ファイルタイプとパーミッションの対応を定義した`FILE_TYPE`と`FILE_PERMISSION`定数を追加

--- 07.ls_object/perm.rb
+++ 07.ls_object/perm.rb
@@ -3,4 +3,23 @@
 # frozen_string_literal: true
 
 class Perm
+  FILE_TYPE = {
+    'fifo' => 'p',
+    'characterSpecial' => 'c',
+    'directory' => 'd',
+    'blockSpecial' => 'b',
+    'file' => '-',
+    'link' => 'l',
+    'socket' => 's'
+  }.freeze
+  FILE_PERMISSION = {
+    0 => '---',
+    1 => '--x',
+    2 => '-w-',
+    3 => '-wx',
+    4 => 'r--',
+    5 => 'r-x',
+    6 => 'rw-',
+    7 => 'rwx'
+  }.freeze
 end
