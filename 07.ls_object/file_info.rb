8add40bf4309da2d46f1a880297980eefa84966d
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-15 14:31:53 +0900

    FileInfoクラスに各情報を取得するメソッドを追加 機能追加
    [目的]
    - ファイルの情報を取得する
    
    [変更内容]
    - `initialize`でファイル名を受け取り、`File::Stat`オブジェクトをインスタンス変数に保持
    - モード、リンク数、オーナー、グループ、サイズ、更新日時などの個別の情報を返すpublicメソッドを実装
    - `-l`オプションが有効な場合にシンボリックリンクの表示を整形する`file_type`メソッドを実装

--- 07.ls_object/file_info.rb
+++ 07.ls_object/file_info.rb
@@ -2,5 +2,45 @@
 
 # frozen_string_literal: true
 
+require_relative 'ls_option'
+require_relative 'perm'
+require 'etc'
+
 class FileInfo
+  def initialize(filename)
+    @stat = File.lstat(filename)
+  end
+
+  def file_mode
+    perm = Perm.new
+    perm.trans_type_and_permission(@stat)
+  end
+
+  def link
+    @stat.nlink
+  end
+
+  def owner
+    Etc.getpwuid(@stat.uid).name
+  end
+
+  def group
+    Etc.getgrgid(@stat.gid).name
+  end
+
+  def bytesize
+    @stat.size
+  end
+
+  def update_time
+    @stat.mtime.strftime('%-m月 %e %H:%M')
+  end
+
+  def file_type(filename, options)
+    if options.long_format?
+      File.symlink?(filename) ? "#{filename} -> #{File.readlink(filename)}" : filename
+    else
+      filename
+    end
+  end
 end
