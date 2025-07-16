eb67ddea6af996e6ed4afc90cde6d86e9a906877
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-16 09:54:26 +0900

    '-l'コマンドの有無によって取得する情報を変更する機能の実装 機能追加
    [目的]
    - ファイル名リストを、コマンドラインオプションに応じて、表示用のデータ構造（詳細情報ハッシュまたはファイル名文字列）に変換
    
    [変更内容]
    - ファイル名リストをループ処理し、ファイルごとに`FileInfo`オブジェクトを生成
    - `-l`オプションが有効な場合、合計ブロック数を計算し、モード、リンク数、オーナーなどの詳細情報をハッシュとして収集
    - `-l`オプションが無効な場合、ファイル名のみを収集
    - 変換後の情報を`file_infos`配列に格納

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -10,3 +10,22 @@ options = LsOption.new(ARGV)
 flags = options.all? ? File::FNM_DOTMATCH : 0
 filenames = Dir.glob('*', flags)
 filenames.reverse! if options.reverse?
+total_blocks = 0
+
+file_infos = filenames.map do |filename|
+  stats = FileInfo.new(filename)
+  if options.long_format?
+    total_blocks += stats.blocks
+    {
+      mode: stats.file_mode,
+      link: stats.link,
+      owner: stats.owner,
+      group: stats.group,
+      size: stats.bytesize,
+      time: stats.update_time,
+      type: stats.file_type(filename, options)
+    }
+  else
+    stats.file_type(filename, options)
+  end
+end
