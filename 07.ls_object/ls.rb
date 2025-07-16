6302f727e3dc6d6ad2a136572ede0ab45ef0ebd9
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-16 10:05:18 +0900

    形式を整えて出力する機能の実装 機能追加
    [目的]
    - コマンドラインオプションに応じて、最終的な出力形式を整形し、表示する
    
    [変更内容]
    - 詳細フォーマット（-l）と標準の複数カラムフォーマットを整形するメソッド(`long_format_lines`、`format_filenames_table`)を追加
    - ファイル情報をループ処理する際に、`Width`クラスを使って各カラムの最大表示幅を計算する処理を統合
    - `-l`オプションの有無に基づき、適切なフォーマット処理を呼び出し、最終的な結果をコンソールに出力するロジックを実装

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -6,14 +6,43 @@ require_relative 'ls_option'
 require_relative 'file_info'
 require_relative 'width'
 
+def format_filenames_table(filenames, max_widths, columns)
+  rows = (filenames.size.to_f / columns).ceil
+  filename_table = Array.new(rows) { Array.new(columns) }
+
+  filenames.each_with_index do |name, index|
+    col, row = index.divmod(rows)
+    filename_table[row][col] = name.ljust(max_widths + 1)
+  end
+
+  filename_table
+end
+
+def long_format_lines(file_infos, max_widths)
+  file_infos.map do |info|
+    [
+      info[:mode],
+      info[:link].to_s.rjust(max_widths[:link]),
+      info[:owner].to_s.ljust(max_widths[:owner]),
+      info[:group].to_s.ljust(max_widths[:group]),
+      "#{info[:size].to_s.rjust(max_widths[:size])} ",
+      info[:time],
+      info[:type]
+    ].join(' ')
+  end
+end
+
 options = LsOption.new(ARGV)
 flags = options.all? ? File::FNM_DOTMATCH : 0
 filenames = Dir.glob('*', flags)
 filenames.reverse! if options.reverse?
 total_blocks = 0
 
+widths = Width.new
+
 file_infos = filenames.map do |filename|
   stats = FileInfo.new(filename)
+  widths.update_max_widths(stats)
   if options.long_format?
     total_blocks += stats.blocks
     {
@@ -28,4 +57,16 @@ file_infos = filenames.map do |filename|
   else
     stats.file_type(filename, options)
   end
+end
+
+max_widths = widths.max_widths
+
+if options.long_format?
+  puts "合計 #{total_blocks}"
+  puts long_format_lines(file_infos, max_widths)
+else
+  column = 3
+  max_widths = filenames.map(&:length).max
+  filename_lines = format_filenames_table(file_infos, max_widths, column)
+  filename_lines.each { |row| puts row.compact.join }
 end
