6567b5b9e6e44c2eaab411791070714901be4c2b
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-16 09:49:10 +0900

    コマンドラインオプションに基づいてファイルリストの取得とソート機能を実装 機能追加
    [目的]
    - オプションに対応したファイルリストの取得を行う
    
    [変更内容]
    - `LsOption`を使い、コマンドライン引数（-a, -r）を解釈する処理を実装
    - `-a`オプションが指定された場合に隠しファイルを含めるため、`Dir.glob`に`File::FNM_DOTMATCH`フラグを渡す
    - `-r`オプションが指定された場合にファイルリストを逆順にソートする処理を追加

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
 
 # frozen_string_literal: true
+
+require_relative 'ls_option'
+require_relative 'file_info'
+require_relative 'width'
+
+options = LsOption.new(ARGV)
+flags = options.all? ? File::FNM_DOTMATCH : 0
+filenames = Dir.glob('*', flags)
+filenames.reverse! if options.reverse?
