bd31d9cbe0c3f16347aa53ecdd64d56c7698ed92
itagaki <itagaki_shun@vitallead.co.jp>
2025-07-16 09:39:48 +0900

    Widthクラスに最大幅を返す機能を実装 機能追加
    [目的]
    - 各カラム（リンク数、オーナー、グループ、サイズ）の最大幅を計算・保持する責務をカプセル化
    
    [変更内容]
    - `initialize`で、各カラムの最大幅を保持するハッシュを初期化するよう実装
    - ファイル情報を受け取り、各カラムの最大幅を更新する`update_max_widths`メソッドを実装

--- 07.ls_object/width.rb
+++ 07.ls_object/width.rb
@@ -3,4 +3,21 @@
 # frozen_string_literal: true
 
 class Width
+  attr_reader :max_widths
+
+  def initialize
+    @max_widths = {
+      link: 0,
+      owner: 0,
+      group: 0,
+      size: 0
+    }
+  end
+
+  def update_max_widths(stats)
+    @max_widths[:link] = [@max_widths[:link], stats.link.to_s.length].max
+    @max_widths[:owner] = [@max_widths[:owner], stats.owner.to_s.length].max
+    @max_widths[:group] = [@max_widths[:group], stats.group.to_s.length].max
+    @max_widths[:size] = [@max_widths[:size], stats.bytesize.to_s.length].max
+  end
 end
